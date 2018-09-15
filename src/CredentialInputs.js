d35765953bd3e26668b2c2ec678010796fb80d19
alfred <alfred@nurd.nu>
2018-09-15 12:18:04 +0000

    store credentials in localstorage

--- src/CredentialInputs.js
+++ src/CredentialInputs.js
@@ -4,15 +4,15 @@ class CredentialInputs extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        keyId: '',
-        secretKey: ''
+        keyId: window.localStorage.getItem('keyId') || '',
+        secretKey: window.localStorage.getItem('secretKey') || ''
       };
     }
 
     render() {
         return <span className="sessionist">
-            <label>Key ID:</label> <input type="text" value={this.state.keyId} onChange={event => this.setState({ keyId: event.target.value })} />
-            <label>Secret key:</label> <input type="text" value={this.state.secretKey} onChange={event => this.setState({ secretKey: event.target.value })} />
+            <label>Key ID:</label> <input type="text" value={this.state.keyId} onChange={event => { window.localStorage.setItem('keyId', event.target.value); this.setState({ keyId: event.target.value })}} />
+            <label>Secret key:</label> <input type="text" value={this.state.secretKey} onChange={event => { window.localStorage.setItem('secretKey', event.target.value); this.setState({ secretKey: event.target.value })}} />
         </span>
     }
 }
